e99b0f859c57795b0eedfc2c1bfb8f770b14c2f4
Sntax <andrewjrhill@gmail.com>
2015-09-11 21:32:22 +0200

    Mast elements and styles.

--- src/app/js/scripts.js
+++ src/app/js/scripts.js
@@ -1,3 +1,21 @@
 /* ===================================================== */
 /* Component:                  Custom page functionality */
 /* ===================================================== */
+
+var era = {
+  init: function() {
+    era.centerMastFeature();
+  },
+
+  centerMastFeature: function() {
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var offsetTop = $('.mast .feature').offset().top;
+    var totalHeight = $('.mast .feature').height();
+
+    $('.mast .feature').css({
+      'margin-top': -(totalHeight/2)
+    });
+  }
+};
+
+era.init();
