3bc01d6d36e1795eba0ebb97040294b872de5997
Sntax <andrewjrhill@gmail.com>
2015-09-25 20:24:52 +0200

    Removing svg to xmlns function. Manually implementing SVGs instead for improved performance.

--- era-theme/js/scripts.js
+++ era-theme/js/scripts.js
@@ -14,7 +14,6 @@ jQuery(document).ready(function($){
     // Initialize all functions
     init: function(){
       eraCapeTown.smoothScroll();
-      eraCapeTown.svgToXmlns();
       eraCapeTown.hamburgerMenu(
         $('.mobile-button, nav.mobile ul li a'),
         'rgba(255, 255, 255, 1)',
@@ -33,35 +32,21 @@ jQuery(document).ready(function($){
       });
     },
 
-    // Converts SVG Imports to base XMLNS to allow for direct path styling
-    svgToXmlns: function(){
-      $('img.svg').each(function(){
-        var img = $(this);
-        var imgID = img.attr('id');
-        var imgClass = img.attr('class');
-        var imgURL = img.attr('src');
-
-        $.get(imgURL, function(data){
-          var svg = $(data).find('svg');
-          if (typeof imgID !== 'undefined'){
-            svg = svg.attr('id', imgID);
-          }
-          if (typeof imgClass !== 'undefined'){
-            svg = svg.attr('class', imgClass+' replaced-svg');
-          }
-          svg = svg.removeAttr('xmlns:a');
-          img.replaceWith(svg);
-        }, 'xml');
-      });
-    },
-
     hamburgerMenu: function(element, oldColour, newColour){
-      element.on('mouseover', function(e){
-        element.find('path').css({fill: newColour, transition: '0.6s'});
-      });
-      element.on('mouseout', function(e){
-        element.find('path').css({fill: oldColour, transition: '0.6s'});
-      });
+      element.hover(
+        function() {
+          element.find('path').css({
+            fill: newColour,
+            transition: '0.6s'
+          });
+        },
+        function() {
+          element.find('path').css({
+            fill: oldColour,
+            transition: '0.6s'
+          });
+        }
+      );
       element.on('click', function(e){
         if ($('nav.mobile ul').is(':visible')) {
           $('nav.mobile ul').slideUp();
