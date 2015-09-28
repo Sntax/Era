60316c49ec90cde9cac9089c2257b7a732ab756a
Sntax <andrewjrhill@gmail.com>
2015-09-28 23:27:30 +0200

    Final navigation polish

--- era-theme/js/scripts.js
+++ era-theme/js/scripts.js
@@ -6,25 +6,22 @@
 /* ===================================================== */
 
 // Utilize 'no-conflict jQuery' to prevent conflicts within Wordpress
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
   // Maintain scope
   var eraCapeTown = {
 
     // Initialize all functions
-    init: function(){
+    init: function() {
       eraCapeTown.smoothScroll();
-      eraCapeTown.hamburgerMenu(
-        $('.mobile-button, nav.mobile ul li a'),
-        'rgba(255, 255, 255, 1)',
-        'rgba(0, 168, 255, 1)'
-      );
+      eraCapeTown.hamburgerMenu();
+      eraCapeTown.navigation();
     },
 
     // Scrolls to a section on click
-    smoothScroll: function(){
+    smoothScroll: function() {
       var root = $('html, body');
-      $('.sticky-nav a').on('click', function(e){
+      $('.sticky-nav a').on('click', function(e) {
         e.preventDefault();
         root.animate({
           scrollTop: $($.attr(this, 'href')).offset().top
@@ -32,26 +29,37 @@ jQuery(document).ready(function($){
       });
     },
 
-    hamburgerMenu: function(element, oldColour, newColour){
-      element.hover(
-        function() {
-          element.find('path').css({
-            fill: newColour,
-            transition: '0.6s'
-          });
-        },
-        function() {
-          element.find('path').css({
-            fill: oldColour,
-            transition: '0.6s'
-          });
-        }
-      );
-      element.on('click', function(e){
+    hamburgerMenu: function() {
+      var mobileButton = $('.mobile-button');
+      mobileButton.on('click', function(e) {
         if ($('nav.mobile ul').is(':visible')) {
           $('nav.mobile ul').slideUp();
+          if ($(document).scrollTop() === 0) {
+            $('section#sticky-nav').removeClass('active');
+          }
         } else {
           $('nav.mobile ul').slideDown();
+          if (!$('section#sticky-nav').hasClass('active')) {
+            $('section#sticky-nav').addClass('active');
+          }
+        }
+      });
+      mobileButton.hover(
+        function() {
+          mobileButton.addClass('hover');
+        },
+        function() {
+          mobileButton.removeClass('hover');
+        }
+      );
+    },
+
+    navigation: function() {
+      $(window).on('scroll', function() {
+        if ($(document).scrollTop() > 0) {
+          $('section#sticky-nav').addClass('active');
+        } else if ($(document).scrollTop() === 0) {
+          $('section#sticky-nav').removeClass('active');
         }
       });
     }
