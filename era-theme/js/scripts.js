9e9453e7a55268f9e59950e8e96973b0cfae3d87
Sntax <andrewjrhill@gmail.com>
2015-10-05 12:03:42 +0200

    Primary accordion functionality.

--- era-theme/js/scripts.js
+++ era-theme/js/scripts.js
@@ -13,9 +13,11 @@ jQuery(document).ready(function($) {
 
     // Initialize all functions
     init: function() {
+      jQuery('p:empty').remove();
       eraCapeTown.smoothScroll();
-      eraCapeTown.hamburgerMenu();
-      eraCapeTown.navigation();
+      eraCapeTown.mobileNavigation();
+      eraCapeTown.sticky();
+      eraCapeTown.accordion();
     },
 
     // Scrolls to a section on click
@@ -30,7 +32,8 @@ jQuery(document).ready(function($) {
       });
     },
 
-    hamburgerMenu: function() {
+    // Mobile navigation functionality
+    mobileNavigation: function() {
       var mobileButton = $('.mobile-button');
       mobileButton.on('click', function(e) {
         if ($('nav.mobile ul').is(':visible')) {
@@ -55,7 +58,8 @@ jQuery(document).ready(function($) {
       );
     },
 
-    navigation: function() {
+    // Sticky navigation functionality
+    sticky: function() {
       $(window).on('scroll', function() {
         if ($(document).scrollTop() > 0) {
           $('section#sticky-nav').addClass('active');
@@ -63,7 +67,36 @@ jQuery(document).ready(function($) {
           $('section#sticky-nav').removeClass('active');
         }
       });
+    },
+
+    // Accordion functionality
+    accordion: function(){
+      var trigger = $('.accordion .part h2');
+      var disclosure = $('p');
+      var part = $('.part');
+      var activeElement = $('.part.active p');
+
+      trigger.on('click', function(){
+        if (!$(this).parent(part).hasClass('active')) {
+          if (activeElement !== null) {
+            activeElement.slideUp(600, function(){
+              $(this).parent(part).removeClass('active');
+            });
+          }
+          activeElement = $(this).siblings(disclosure);
+          activeElement.slideDown(600, function(){
+            $(this).parent(part).addClass('active');
+          });
+        } else if ($(this).parent(part).hasClass('active')) {
+          activeElement = $(this).siblings(disclosure);
+          activeElement.slideUp(600, function() {
+            $(this).parent(part).removeClass('active');
+            activeElement = null;
+          });
+        }
+      });
     }
+
   };
   eraCapeTown.init();
 });
