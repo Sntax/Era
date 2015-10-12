6851d05d68d75df6a0a7c90c2c126aac136f9a92
Sntax <andrewjrhill@gmail.com>
2015-10-12 21:55:44 +0200

    Uploading comps

--- era-theme/functions.php
+++ era-theme/functions.php
@@ -54,6 +54,7 @@
 
   add_action('wp_enqueue_scripts', 'era_cape_town_scripts');
 
+  /* Defer parsing of Javascript to improve page performance */
   function defer_parsing_of_js ( $url ) {
     if ( FALSE === strpos( $url, '.js' ) ) return $url;
     if ( strpos( $url, 'jquery.js' ) ) return $url;
